15a9f26670304637da8790b28737564dbef537ad
nkottary <kottarynishanth54@gmail.com>
2019-05-03 12:10:42 +0530

    Use string() instead of phrase_argument()

--- src/Server.jl
+++ src/Server.jl
@@ -336,10 +336,6 @@ struct ProcessedParams
     end
 end
 
-# `x` can only be Expr, Symbol, QuoteNode, T<:Number, or T<:AbstractString
-phrase_argument(x::Union{Expr, Symbol, QuoteNode}) = string(x)
-phrase_argument(x::Union{AbstractString, Number})  = repr(x)
-
 function parse_submission_string(fncall)
     argind = findfirst(isequal('('), fncall)
     name = fncall[1:(argind - 1)]
@@ -350,17 +346,17 @@ function parse_submission_string(fncall)
         for x in parsed_args.args
             if isa(x, Expr) && (x.head == :kw || x.head == :(=)) && isa(x.args[1], Symbol)
                 @assert !haskey(kwargs, x.args[1]) "kwargs must all be unique"
-                kwargs[x.args[1]] = phrase_argument(x.args[2])
+                kwargs[x.args[1]] = string(x.args[2])
                 started_kwargs = true
             else
                 @assert !started_kwargs "kwargs must come after other args"
-                push!(args, phrase_argument(x))
+                push!(args, string(x))
             end
         end
     elseif isa(parsed_args, Expr) && parsed_args.head == :(=) && isa(parsed_args.args[1], Symbol)
-        kwargs[parsed_args.args[1]] = phrase_argument(parsed_args.args[2])
+        kwargs[parsed_args.args[1]] = string(parsed_args.args[2])
     else
-        push!(args, phrase_argument(parsed_args))
+        push!(args, string(parsed_args))
     end
     return name, args, kwargs
 end
