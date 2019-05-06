4a95302782b334ef1f4ef88455772944282794c8
Chris de Graaf <chrisadegraaf@gmail.com>
2019-05-06 05:56:42 -0500

    Skip web tests on Travis PRs

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,12 @@ using Test
 
 include("server.jl")
 include("regedit.jl")
-include("webui.jl")
+
+# Travis CI gets rate limited easily unless we have access to an API key.
+if get(ENV, "TRAVIS", "") == "true" && !haskey(ENV, "GITHUB_API_TOKEN")
+    @info "Skipping web tests on Travis CI (credentials are unavailable)"
+else
+    include("webui.jl")
+end
 
 end
