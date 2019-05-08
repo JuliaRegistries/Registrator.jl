8e802d37266c16238569b6d796f6c383e3dde096
nkottary <kottarynishanth54@gmail.com>
2019-05-08 13:10:40 +0530

    Fix regex to accept Patch notes, add tests

--- test/server.jl
+++ test/server.jl
@@ -4,6 +4,17 @@ using Test
 
 @testset "Server" begin
 
+@testset "accept_regex" begin
+    r = Regex("@JuliaRegistrator $(Registrator.RegServer.accept_regex)")
+    @test match(r, "@JuliaRegistrator register()")[1] == "register()"
+    @test match(r, "@JuliaRegistrator register()\r\n\r\n")[1] == "register()"
+    @test match(r, """@JuliaRegistrator register()
+                Patch notes:
+                Release v0.1.0
+                Fixes bugs
+                """)[1] == "register()"
+end
+
 @testset "parse_submission_string" begin
     @test parse_submission_string("register()") == ("register", [], Dict{Symbol,String}())
     @test parse_submission_string("register(qux)") == ("register", ["qux"], Dict{Symbol,String}())
